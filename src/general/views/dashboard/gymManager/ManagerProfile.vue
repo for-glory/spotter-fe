c91d8317270fe18e905c30acf4fa429a65f1531d
Danylo Levada <chicken931205@outlook.com>
2023-08-10 03:10:35 -0700

    add NotificationItem component for manager profile page

--- src/general/views/dashboard/gymManager/ManagerProfile.vue
+++ src/general/views/dashboard/gymManager/ManagerProfile.vue
@@ -3,10 +3,51 @@
 		class="holder-content ion-padding-horizontal"
 		:class="{ 'holder-content--empty': !loading }"
 	>
-    <div class="banner">
+    <div class="banner d-flex justify-content-between align-items-center">
       <ion-title class="banner__title">Gym Manager Profile</ion-title>
+      <ion-button expand="block">Edit</ion-button>
     </div>
     <div class="content-container">
+      <div class="content-box">
+        <ion-title class="top">Membership Summary</ion-title>
+        <ion-grid>
+          <ion-row>
+            <ion-col size="4">
+              <summary-item title="Total" keyText="New Signs-up" value="14"/>
+            </ion-col>
+            <ion-col size="4">
+              <summary-item title="Total" keyText="Active" value="60"/>
+            </ion-col>
+            <ion-col size="4">
+              <summary-item title="Total" keyText="Experiment membership" value="24"/>
+            </ion-col>
+          </ion-row>
+          <ion-row>
+            <ion-col size="4">
+              <summary-item title="Today's" keyText="Event counts" value="23"/>
+            </ion-col>
+            <ion-col size="4">
+              <summary-item title="Today's" keyText="Event counts" value="13"/>
+            </ion-col>
+          </ion-row>
+        </ion-grid>
+      </div>
+      <div class="content-box">
+        <div >
+          <div class="top d-flex justify-content-between align-items-center">
+            <ion-title>Notification</ion-title>
+            <div>
+              <ion-text>{{ 8 }} task</ion-text>
+              <ion-text>{{ " " + "completed" + " " }}</ion-text>
+              <ion-text>out of {{ 10 }}</ion-text>
+            </div>
+          </div>
+        </div>
+        <div class="notification-box">
+          <notification-item></notification-item>
+          <notification-item></notification-item>
+        </div>
+      </div>
     </div>
 	</div>
 </template>
@@ -34,6 +75,7 @@ import useFacilityId from "@/hooks/useFacilityId";
 // import dayjs from "dayjs";
 import useRoles from "@/hooks/useRole";
 import SummaryItem from "@/general/components/dashboard/SummaryItem.vue";
+import NotificationItem from "@/general/components/dashboard/NotificationItem.vue";
 
 
 const filter = ref<string>('profile');
@@ -74,47 +116,26 @@ const handleClick = (value: string) => {
   }
 }
 .content-container {
-  padding-top: 31px;
+  padding-top: 51px;
   display: flex;
   flex-direction: column;
-  gap: 37px;
+  gap: 62px;
+
+  .top {
+    margin-bottom: 40px;
+  }
 }
 .content-box {
   background-color: var(--gray-700);
-
-  &__membership {
-    padding-top: 34px;
-    padding-left: 30px;
-    padding-right: 90px;
-    padding-bottom: 20px;
-    border-radius: 8px;
-  }
+  padding-top: 34px;
+  padding-left: 31px;
+  padding-right: 31px;
+  padding-bottom: 20px;
+  border-radius: 8px;
 }
-.manager-table {
-  border-top-left-radius: 8px;
-	border-top-right-radius: 8px;
-	border: 1px solid var(--fitnesswhite);
-	background: var(--gray-700);
-	max-width: 700px;
-	padding: 0;
-
-  .table-th {
-		padding: 10px 24px;
-	}
-
-	.table-td {
-		padding: 16px 24px;
-	}
-}
-
-.table-header {
-	border-top-left-radius: 8px;
-	border-top-right-radius: 8px;
-	background-color: var(--main-color);
-	color: var(--gold);
-}
-
-.table-row {
-	border-top: 1px solid var(--fitnesswhite);
+.notification-box {
+  display: flex;
+  flex-direction: column;
+  gap: 17px;
 }
 </style>
