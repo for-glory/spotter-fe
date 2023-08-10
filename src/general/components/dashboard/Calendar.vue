fc00a962ed308a30df158f389b659793effcfb8f
Danylo Levada <chicken931205@outlook.com>
2023-08-10 04:36:08 -0700

    add Calendar component for gym manager profile page

---
+++ src/general/components/dashboard/Calendar.vue
@@ -0,0 +1,242 @@
+<template>
+  <div :class="{ 'calendar--dark': darkBackground }" class="calendar">
+    <ion-datetime
+      ref="calendar"
+      :value="selectedDate"
+      :class="{ 'calendar--dark': darkBackground }"
+      @ion-change="onChange"
+      presentation="date"
+      mode="ios"
+      :min="minData || min ? dayjs(min).toISOString() : undefined"
+      :max="max ? dayjs(max).toISOString() : undefined"
+    />
+    <div v-if="showAdditionalContent" class="calendar__footer">
+      <ion-text class="time__label"> Time</ion-text>
+    </div>
+    <ion-spinner v-if="loading" name="lines" class="spinner" />
+    <base-carousel
+      v-if="!loading && showAdditionalContent"
+      :items="times"
+      width-auto
+    >
+      <template v-slot:default="times">
+        <div
+          :class="{
+            'carousel__item--on-selected': selectedTime === times.item.value,
+          }"
+          class="carousel__item"
+          @click="onSelectTime($event, times.item.paymentTime)"
+        >
+          {{ times.item.value }}
+        </div>
+      </template>
+    </base-carousel>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {
+  DatetimeCustomEvent,
+  IonDatetime,
+  IonText,
+  IonSpinner,
+} from "@ionic/vue";
+import BaseCarousel from "@/general/components/base/BaseCarousel.vue";
+import {
+  defineProps,
+  onMounted,
+  ref,
+  withDefaults,
+  defineEmits,
+  watch,
+} from "vue";
+import dayjs from "dayjs";
+import { computed } from "@vue/reactivity";
+
+const props = withDefaults(
+  defineProps<{
+    showAdditionalContent?: boolean;
+    darkBackground?: boolean;
+    times?: { value: string }[];
+    loading?: boolean;
+    selected?: number | null | object;
+    min?: number;
+    max?: number;
+  }>(),
+  {
+    showAdditionalContent: true,
+    darkBackground: false,
+    loading: false,
+    selected: Date.now(),
+  }
+);
+
+watch(
+  () => props.times,
+  (newVal) => {
+    if (newVal?.length) {
+      selectedTime.value = newVal[0].value;
+    }
+  }
+);
+
+const calendar = ref<typeof IonDatetime | null>(null);
+const selectedDate = ref<string | string[]>(
+  props.selected ? dayjs(props.selected).toISOString() : dayjs().toISOString()
+);
+const selectedTime = ref<string | string[]>("");
+const minData = computed(() => dayjs().format("YYYY-MM-DD"));
+
+const emits = defineEmits<{
+  (e: "change-day", day: string): void;
+  (e: "change-time", time: string, timeValue: string): void;
+}>();
+
+const onChange = (event: DatetimeCustomEvent) => {
+  if (event.detail.value) {
+    selectedDate.value = event.detail.value;
+    emits("change-day", event.detail.value);
+  }
+};
+
+const onSelectTime = (event: CustomEvent, updatedTime: string) => {
+  selectedTime.value = event?.target?.textContent;
+  emits("change-time", updatedTime, event?.target?.textContent);
+};
+
+onMounted(() => {
+  if (props.times?.length) {
+    selectedTime.value = props.times[0].value;
+  }
+
+  if (calendar.value) {
+    const shadow = calendar.value.$el.shadowRoot;
+    const style = document.createElement("style");
+    style.innerHTML = ` .calendar-day {
+          font-size: 16px !important;
+        }
+        .calendar-month-grid {
+          padding: 0 8px !important;
+          grid-template-rows: repeat(5, 47px);
+        }
+
+         .calendar-action-buttons ion-icon {
+          font-size: 24px;
+          color: white !important;
+         }
+
+         .calendar-action-buttons ion-label {
+          margin: 0;
+          font-size: 16px;
+         }
+
+         .calendar-action-buttons {
+          padding-top: 3px;
+          padding-bottom: 6px;
+          align-items: center;
+         }
+
+         .calendar-days-of-week {
+          font-size: 14px !important;
+          color: var(--gray-500) !important;
+         }
+
+         .calendar-next-prev ion-button {
+          width: 32px !important;
+          height: 32px !important;
+         }
+
+         .calendar-next-prev ion-button::part(native) {
+          padding-inline-start: 0 !important;
+          padding-inline-end: 0 !important;
+         }
+
+         .calendar-next-prev > ion-buttons {
+         padding-right: 10px !important;
+          padding-top: 0 !important;
+         }
+
+         :host(.datetime-presentation-date:not(.datetime-prefer-wheel)) {
+          min-height: 350px !important;
+         }
+        `;
+    shadow.appendChild(style);
+  }
+});
+</script>
+
+<style scoped lang="scss">
+.calendar {
+  border-radius: 8px;
+  padding: 5px 0;
+  background: var(--gray-700);
+  position: relative;
+
+  &--dark {
+    background: var(--gray-800);
+  }
+
+  &__footer {
+    font-weight: 500;
+    font-size: 16px;
+    margin: 0 22px;
+    padding: 12px 0;
+    border-top: 1px solid var(--gray-600);
+  }
+}
+
+.button-time {
+  --background: transparent;
+  --color: var(--ion-color-white);
+  border: 1px solid var(--gray-600);
+  border-radius: 8px;
+  font-weight: 400;
+  font-size: 14px;
+
+  &.selected {
+    border-color: var(--gold);
+  }
+}
+
+ion-datetime {
+  --background: var(--gray-700);
+  --title-color: var(--ion-color-white);
+  color: var(--ion-color-white);
+  margin: 0 auto;
+  align-content: flex-start;
+
+  &.calendar--dark {
+    --background: var(--gray-800);
+  }
+}
+
+.carousel {
+  &__item {
+    display: inline-block;
+    margin-bottom: 12px;
+    font-weight: 400;
+    font-size: 14px;
+    padding: 12px 16px;
+    border: 1px solid var(--gray-600);
+    border-radius: 8px;
+    color: var(--gray-500);
+
+    &--on-selected {
+      border-color: var(--gold);
+      color: var(--ion-color-white);
+    }
+  }
+}
+
+.time {
+  &__label {
+    padding-top: 10px;
+    margin: 12px 0;
+  }
+}
+.spinner {
+  display: block;
+  margin: 6px auto 22px;
+  --color: var(--ion-color-white);
+}
+</style>
--- src/general/views/dashboard/gymManager/ManagerProfile.vue
+++ src/general/views/dashboard/gymManager/ManagerProfile.vue
@@ -8,44 +8,82 @@
       <ion-button expand="block">Edit</ion-button>
     </div>
     <div class="content-container">
-      <div class="content-box">
-        <ion-title class="top">Membership Summary</ion-title>
-        <ion-grid>
-          <ion-row>
-            <ion-col size="4">
-              <summary-item title="Total" keyText="New Signs-up" value="14"/>
-            </ion-col>
-            <ion-col size="4">
-              <summary-item title="Total" keyText="Active" value="60"/>
-            </ion-col>
-            <ion-col size="4">
-              <summary-item title="Total" keyText="Experiment membership" value="24"/>
-            </ion-col>
-          </ion-row>
-          <ion-row>
-            <ion-col size="4">
-              <summary-item title="Today's" keyText="Event counts" value="23"/>
-            </ion-col>
-            <ion-col size="4">
-              <summary-item title="Today's" keyText="Event counts" value="13"/>
-            </ion-col>
-          </ion-row>
-        </ion-grid>
-      </div>
-      <div class="content-box">
-        <div >
-          <div class="top d-flex justify-content-between align-items-center">
-            <ion-title>Notification</ion-title>
-            <div>
-              <ion-text>{{ 8 }} task</ion-text>
-              <ion-text>{{ " " + "completed" + " " }}</ion-text>
-              <ion-text>out of {{ 10 }}</ion-text>
-            </div>
+      <div class="profile-field">
+        <div class="contact-field">
+          <ion-img src="assets/manager-avatar.png"></ion-img>
+          <ion-label class="name">
+            {{"Ray Espinal"}}
+            <ion-icon slot="icon-only" src="assets/icon/arrow-down.svg"></ion-icon>
+          </ion-label>
+          <ion-text class="contact">{{"Gym Manager"}}</ion-text>
+          <ion-text class="contact">{{"gymmanager@spotterfitness.com"}}</ion-text>
+          <ion-text class="contact">{{"(+1)70 8750 9216"}}</ion-text>
+        </div>
+        <div class="data-box d-flex align-items-center justify-content-between">
+          <div class="d-flex-col align-items-center">
+            <ion-text>{{40}}</ion-text>
+            <ion-text class="field-label">Age</ion-text>
+          </div>
+          <div class="vertical-line"/>
+          <div class="d-flex-col align-items-center">
+            <ion-text>{{"Full-Time"}}</ion-text>
+            <ion-text class="field-label">Employment type</ion-text>
+          </div>
+          <div class="vertical-line"/>
+          <div class="d-flex-col align-items-center">
+            <ion-text>{{"Manager"}}</ion-text>
+            <ion-text class="field-label">Position</ion-text>
           </div>
         </div>
-        <div class="notification-box">
-          <notification-item></notification-item>
-          <notification-item></notification-item>
+        <ion-title class="calendar-title">Calendar</ion-title>
+        <calendar
+          :selected="selectedDate"
+          @change-day="dayChanged"
+          :showAdditionalContent="false"
+          :min="props?.options?.min"
+          :max="props?.options?.max"
+        />
+      </div>
+      <div class="data-field">
+        <div class="content-box">
+          <ion-title class="top">Membership Summary</ion-title>
+          <ion-grid>
+            <ion-row>
+              <ion-col size="4">
+                <summary-item title="Total" keyText="New Signs-up" value="14"/>
+              </ion-col>
+              <ion-col size="4">
+                <summary-item title="Total" keyText="Active" value="60"/>
+              </ion-col>
+              <ion-col size="4">
+                <summary-item title="Total" keyText="Experiment membership" value="24"/>
+              </ion-col>
+            </ion-row>
+            <ion-row>
+              <ion-col size="4">
+                <summary-item title="Today's" keyText="Event counts" value="23"/>
+              </ion-col>
+              <ion-col size="4">
+                <summary-item title="Today's" keyText="Event counts" value="13"/>
+              </ion-col>
+            </ion-row>
+          </ion-grid>
+        </div>
+        <div class="content-box">
+          <div >
+            <div class="top d-flex justify-content-between align-items-center">
+              <ion-title>Notification</ion-title>
+              <div>
+                <ion-text>{{ 8 }} task</ion-text>
+                <ion-text>{{ " " + "completed" + " " }}</ion-text>
+                <ion-text>out of {{ 10 }}</ion-text>
+              </div>
+            </div>
+          </div>
+          <div class="notification-box">
+            <notification-item></notification-item>
+            <notification-item></notification-item>
+          </div>
         </div>
       </div>
     </div>
@@ -58,7 +96,8 @@ import {
   IonSpinner,
   IonGrid,
   IonText,
-  IonTitle
+  IonTitle,
+  IonIcon
 } from "@ionic/vue";
 import { EntitiesEnum } from "@/const/entities";
 import {
@@ -74,9 +113,13 @@ import useId from "@/hooks/useId";
 import useFacilityId from "@/hooks/useFacilityId";
 // import dayjs from "dayjs";
 import useRoles from "@/hooks/useRole";
+import {
+  DatePickerModalResult,
+  DatePickerModalOptions,
+} from "@/interfaces/DatePickerModal";
 import SummaryItem from "@/general/components/dashboard/SummaryItem.vue";
 import NotificationItem from "@/general/components/dashboard/NotificationItem.vue";
-
+import Calendar from "@/general/components/dashboard/Calendar.vue";
 
 const filter = ref<string>('profile');
 
@@ -85,12 +128,17 @@ const { id: myFacilityId } = useFacilityId();
 const { role: myRole } = useRoles();
 
 const { id } = JSON.parse(localStorage.getItem("user") || "{}");
+const props = ref<DatePickerModalOptions>();
+const selectedDate = ref<number>(Date.now());
 
 const router = useRouter();
 
 const handleClick = (value: string) => {
 	filter.value = value;
 }
+const dayChanged = (event: any) => {
+  selectedDate.value = new Date(event).getTime();
+};
 
 </script>
 
@@ -118,11 +166,44 @@ const handleClick = (value: string) => {
 .content-container {
   padding-top: 51px;
   display: flex;
-  flex-direction: column;
   gap: 62px;
 
   .top {
     margin-bottom: 40px;
+  }
+  .profile-field {
+    width: 350px;
+    display: flex;
+    flex-direction: column;
+    gap:22px;
+
+    .calendar-title {
+      font: 20px/1 var(--ion-font-family);
+      color: #ffffff6a;
+      padding: 0;
+    }
+  }
+  .data-field {
+    flex: auto;
+  }
+  .contact-field {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 8px;
+
+    .name {
+      font: 500 24px/1 var(--ion-font-family);
+      color: var(--fitnesswhite);
+    }
+    .contact{
+      font: 16px/1 var(--ion-font-family);
+      color: var(--gray-400);
+    }
+
+    ion-img {
+      width: 86px;
+    }
   }
 }
 .content-box {
@@ -132,10 +213,37 @@ const handleClick = (value: string) => {
   padding-right: 31px;
   padding-bottom: 20px;
   border-radius: 8px;
+  margin-bottom: 18px;
+}
+.data-box {
+  background-color: var(--gray-700);
+  padding-top: 15px;
+  padding-bottom: 15px;
+  padding-right: 28px;
+  padding-left: 28px;
+  border-radius: 8px;
+
+  .field-label {
+    font: 300 14px/1 var(--ion-font-family);
+    color: var(--gray-400);
+    padding-top: 4px;
+  }
 }
 .notification-box {
   display: flex;
   flex-direction: column;
   gap: 17px;
 }
+.d-flex-col {
+  display: flex;
+  flex-direction: column;
+}
+.vertical-line {
+  border: solid;
+  border-width: 2px;
+  border-radius: 2px;
+  min-height: 32px;
+  border-color: var(--main-color);
+}
+
 </style>
