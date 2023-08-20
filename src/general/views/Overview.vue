0ddc9c9f3e628e193729a4c72cf5a644385a8156
Danylo Levada <chicken931205@outlook.com>
2023-08-20 11:51:32 -0700

    add Revenue section to Overview Page

--- src/general/views/Overview.vue
+++ src/general/views/Overview.vue
@@ -13,6 +13,43 @@
     </template>
     <template #content>
       <div class="profile">
+        <div>
+          <ion-title class="title">Revenue</ion-title>
+          <div class="block">
+            <div class="revenue">
+              <div class="type">Total revenue</div>
+              <div class="period">Today</div>
+              <div class="percent">
+                0.8%
+                <ion-icon class="arrow" src="assets/icon/arrow-down-light.svg" />
+              </div>
+              <div class="chain">$3,689,076</div>
+            </div>
+            <div class="revenue">
+              <div>
+                <div class="type">Quarterly revenue</div>
+                <div class="period">Earned</div>
+                <div class="percent">70%</div>
+                <div class="chain">
+                  $1068<span class="period">/ day</span>
+                </div>
+              </div>
+              <ion-icon
+                class="vector"
+                src="assets/icon/chat/Vector.svg"
+              />
+            </div>
+            <div class="revenue">
+              <div class="type">Earned revenue</div>
+              <div class="period">This year</div>
+              <div class="percent">
+                0.8%
+                <ion-icon class="arrow" src="assets/icon/call_made.svg" />
+              </div>
+              <div class="chain">$689,076</div>
+            </div>
+          </div>
+        </div>
       </div>
     </template>
   </base-layout>
@@ -225,10 +262,7 @@ profileDeleted(() => {
 </script>
 <style scoped lang="scss">
 .profile {
-  text-align: center;
   padding: 16px 24px;
-  display: flex;
-  flex-direction: column;
 
   &__head {
     text-align: center;
@@ -277,10 +311,20 @@ profileDeleted(() => {
   &:deep(.choose-place__label) {
     display: flex;
   }
+
+  .title {
+    padding: 8px 0px;
+    font-size: 1.6rem;
+    line-height: 1.3;
+    font-weight: 400;
+  }
+}
+.background-content {
+  background-color: #202020;
 }
 .header {
   padding: 16px 7px;
-  
+
   &__title {
     padding: 0;
     font-family: "Yantramanav", serif;
@@ -297,5 +341,35 @@ profileDeleted(() => {
     max-height: 28px;
   }
 }
+.revenue {
+  margin: 0.3rem 0.3rem;
+  padding: 1rem 0 1rem 1rem;
+  border: 1px solid white;
+  border-radius: 8px;
 
+  .type {
+    font-size: 1rem;
+    color: white;
+    padding-bottom: 10px;
+  }
+  .percent {
+    font-size: 1.625rem;
+    color: #2ed47a;
+    padding-bottom: 5px;
+  }
+  .chain {
+    font-size: 1.625rem;
+    color: white;
+  }
+  .arrow {
+    padding-left: 0.6rem;
+  }
+}
+.block {
+  width: 100%;
+  background-color: #262626;
+  border-radius: 4px;
+  margin-bottom: 1rem;
+  padding: 1rem 1rem 1rem 1rem;
+}
 </style>
