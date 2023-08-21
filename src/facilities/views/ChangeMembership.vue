41ccf0aa3e19d70595089d97ef89a4a97931c840
Danylo Levada <chicken931205@outlook.com>
2023-08-21 05:04:35 -0700

    update change membership page

--- src/facilities/views/ChangeMembership.vue
+++ src/facilities/views/ChangeMembership.vue
@@ -1,5 +1,8 @@
 <template>
   <base-layout>
+    <template #header>
+      <page-header back-btn title="Change Membership" @back="onBack" />
+    </template>
     <template #content>
       <ion-spinner
         v-if="loading || plansLoading"
@@ -7,70 +10,109 @@
         class="spinner"
       />
       <div class="membership" v-else>
-        <ion-radio-group class="plans" v-model="selectedPlanId">
-          <ion-slides 
-            ref="slide"
+        <ion-radio-group v-model="selectedPlanId">
+          <ion-item
+            lines="none"
+            class="radiobutton"
+            v-for="plan in plans"
+            :key="plan.id"
+            @click="selectProduct(plan)"
+            :class="{
+              'item-radio-checked':
+                plan.owned && dayjs(plan.expiryDate).isAfter(dayjs()),
+            }"
+            :disabled="plan.owned && dayjs(plan.expiryDate).isAfter(dayjs())"
           >
-            <ion-slide
-              v-for="plan in plans"
-              :key="plan.id"
-            >
-              <ion-item
-                lines="none"
-                class="radiobutton"
-                @click="selectMembership(plan)"
-              >
-                <div class="radiobutton__block">
-                  <div class="radiobutton__icon">
-                    <ion-icon src="assets/icon/medal.svg" 
-                      :class="plan.tier === 'BRONZE' ? 'bronze' : plan.tier === 'SILVER' ? 'silver' : 'gold'" 
-                    />
-                  </div>
-                  <div class="radiobutton__description">
-                    <ion-label class="radiobutton__label">
-                      {{ plan.title }}
-                    </ion-label>
+            <div class="radiobutton__block">
+              <div class="radiobutton__icon">
+                <ion-icon src="assets/icon/medal.svg" 
+                  :class="plan?.tier === 'BRONZE' ? 'bronze' : plan?.tier === 'SILVER' ? 'silver' : 'gold'"
+                />
+              </div>
+              <div class="radiobutton__description">
+                <ion-label class="radiobutton__label">
+                  {{ plan.title }}
+                  <span class="status-text" v-if="currentPlan?.tier.toLowerCase() === plan?.tier.toLowerCase() || plan?.tier === 'GOLD'">
+                    {{currentPlan?.tier.toLowerCase() === plan?.tier.toLowerCase() ? 'Current' : plan?.tier === 'GOLD' ? 'TOP' : '' }}
+                  </span>
+                </ion-label>
 
-                    <ion-text class="radiobutton__cost"
-                      >${{ plan.prices[0].price / 100 }}
-                      <span>
-                        /{{ plan.tier === 'GOLD' ? 'for first location' : 'per location'}}
-                      </span>
-                    </ion-text>
-                    <ul>
-                      <li
-                        class="accessibility"
-                        v-for="(benefit, idx) in plan?.benefits"
-                        :key="idx"
-                      >
-                        <div>
-                          <ion-icon src="assets/icon/accessibility.svg" />
-                        </div>
-                        <div>
-                          <ion-text>{{ benefit?.description }}</ion-text>
-                        </div>
-                      </li>
-                    </ul>
-                  </div>
-                  <div class="tax-text">
-                    * Taxes and fees may apply.
-                  </div>
-                </div>
-                <ion-radio :value="plan.id" slot="end"></ion-radio>
-              </ion-item>
-            </ion-slide>
-          </ion-slides>
-          <ion-icon class="prev" src="assets/icon/arrow-back.svg" @click="prev"></ion-icon>
-          <ion-icon class="next" src="assets/icon/arrow-next.svg" @click="next"></ion-icon>
+                <ion-text class="radiobutton__cost"
+                  >${{ plan?.prices[0].price / 100 }}
+                  <span>
+                    /{{ plan?.tier === 'GOLD' ? 'for first location' : 'per location'}}
+                  </span>
+                </ion-text>
+                <ul>
+                  <li
+                    class="accessibility"
+                    v-for="(benefit, idx) in plan?.benefits"
+                    :key="idx"
+                  >
+                    <ion-icon src="assets/icon/accessibility.svg" />
+                    {{ benefit?.description }}
+                  </li>
+                  <!-- TODO add inaccessible features -->
+                  <!-- <li>
+                    <ion-icon src="assets/icon/inaccessible.svg" />
+                    Top Features
+                  </li> -->
+                </ul>
+              </div>
+            </div>
+            <ion-radio :value="plan.id" slot="end"></ion-radio>
+          </ion-item>
         </ion-radio-group>
-        <div class="membership-buttons d-flex align-items-center justify-content-between">
-          <ion-button id="change" @click="onChangeMembership">Change Membership</ion-button>
-          <ion-button id="cancel" >Cancel Membership</ion-button> 
+      </div>
+    </template>
+    <template #footer>
+      <div class="holder-button">
+        <div class="checkbox">
+          <ion-checkbox
+            mode="ios"
+            :modelValue="isAgreed"
+            @update:modelValue="isAgreed = $event"
+          >
+          </ion-checkbox>
+          <ion-label>
+            I confirm that I read and accept
+            <ion-text
+              @click.stop="
+                showAgreement(
+                  'https://app.termly.io/document/privacy-policy/90cdb569-0bff-4241-9edd-7d3584f78bfc'
+                )
+              "
+              color="primary"
+            >
+              Privacy Policy
+            </ion-text>
+            and
+            <ion-text
+              @click.stop="
+                showAgreement(
+                  'https://app.termly.io/document/terms-of-use-for-saas/60ed2ae3-4eee-4617-b3d7-957ac10623b7'
+                )
+              "
+              color="primary"
+            >
+              Terms of Use
+            </ion-text>
+          </ion-label>
         </div>
+        <ion-button
+          expand="block"
+          @click="purchase"
+          :disabled="!isAgreed || isLoading"
+        >
+          <ion-spinner name="lines" v-if="isLoading"></ion-spinner>
+          <template v-else>Continue</template>
+        </ion-button>
+      </div>
+      <div>
+        {{ errorMessage }}
       </div>
     </template>
   </base-layout>
-  <change-membership ref="changeMembershipModal" @confirm="confirmChange"/>
 </template>
 
 <script setup lang="ts">
@@ -86,8 +128,6 @@ import {
   IonSpinner,
   onIonViewDidLeave,
   IonCheckbox,
-  IonSlide,
-  IonSlides
 } from "@ionic/vue";
 import BaseLayout from "@/general/components/base/BaseLayout.vue";
 import PageHeader from "@/general/components/blocks/headers/PageHeader.vue";
@@ -111,7 +151,6 @@ import { setAuthItemsFromMe } from "@/router/middleware/auth";
 import useSubscription from "@/hooks/useSubscription";
 import { Capacitor } from '@capacitor/core';
 import { EntitiesEnum } from "@/const/entities";
-import ChangeMembership from "@/general/components/modals/confirmations/ChangeMembership.vue"
 
 const router = useRouter();
 const route = useRoute();
@@ -124,10 +163,7 @@ const products = ref<any[]>([]);
 const selectedPlanId = ref<string | number | boolean | undefined>(undefined);
 const isAgreed = ref<boolean>(false);
 const isLoading = ref<boolean>(false);
-const slide = ref<typeof IonSlides | null>();
 const currentPlan = ref<any>();
-const { type: currentSubscriptionType } = useSubscription();
-const changeMembershipModal = ref<typeof ChangeMembership | null>(null);
 
 const typeValue = computed(() => {
   if (role === RoleEnum.Trainer) {
@@ -146,7 +182,6 @@ onMounted(async () => {
     process.env.VUE_APP_IAP_RECEIPT_VALIDATION_URL + `/${id}`;
   // InAppPurchase2.validator = validationUrl;
   InAppPurchase2.applicationUsername = () => id;
-  console.log({InAppPurchase2});
 
   onPlansResult(async ({ data }) => {
     plans.value = data?.plans?.data.reduce((acc: any[], cur: any) => {
@@ -164,7 +199,7 @@ onMounted(async () => {
       }
       return acc;
     }, []);
-    currentPlan.value = plans.value.filter((plan) => plan.tier.toLowerCase() === currentSubscriptionType.toLowerCase() && plan);
+    currentPlan.value = plans.value.filter((plan) => plan.tier.toLowerCase() === currentSubscriptionType.toLowerCase() && plan)[0];
     InAppPurchase2.ready(() => {
       products.value = InAppPurchase2.products;
       console.log("set up listeners after ready");
@@ -180,7 +215,6 @@ const registerProducts = async () => {
       type: InAppPurchase2.PAID_SUBSCRIPTION,
     });
   });
-  console.log({InAppPurchase2});
   setupListeners();
   InAppPurchase2.refresh();
 };
@@ -253,6 +287,45 @@ const setupListeners = async () => {
 
   InAppPurchase2.error(errorEvent);
 };
+const purchase = () => {
+  isLoading.value = true;
+  if (Capacitor.isNativePlatform()) {
+    InAppPurchase2.order(selectedItem.value)
+      .then(() => {
+        isLoading.value = false;
+        console.log(
+          "entered into Apple/Google pop up with the purchase confirmation"
+        );
+      })
+      .error((error: any) => {
+        isLoading.value = false;
+        errorMessage.value = `Failed to purchase: ${error}`;
+      });
+  } else {
+    console.log('Web platform', selectedItem.value);
+    router.push({
+      name: EntitiesEnum.SubscriptionPaymentMethod,
+      params: { subscriptionId: selectedItem.value.product_id },
+      query: {
+          facilityId: route?.query?.facilityId || "",
+        },
+    });
+  }
+};
+
+const selectProduct = (plan: any) => {
+  if (Capacitor.isNativePlatform()) {
+    selectedItem.value = InAppPurchase2.products
+      .filter((value) => {
+        return value.id === plan.subscriptionPlan.product_id;
+      })
+      .pop();
+  } else {
+    console.log('Web platform');
+    console.log('plan.subscriptionPlan', plan.subscriptionPlan);
+    selectedItem.value = plan.subscriptionPlan
+  }
+};
 
 const {
   loading,
@@ -262,6 +335,7 @@ const {
   fetchPolicy: "no-cache",
 });
 
+const { type: currentSubscriptionType } = useSubscription();
 gotMyProfile(({ data }) => {
   if (data.me.currentSubscription !== currentSubscriptionType) {
     setAuthItemsFromMe(data.me);
@@ -286,282 +360,14 @@ const onBack = () => {
   router.go(-1);
 };
 
-const prev = () => {
-	slide.value?.$el.slidePrev();
-}
-const next = () => {
-	slide.value?.$el.slideNext();
-}
-
-
 const showAgreement = async (url: string) => {
   await Browser.open({ url });
 };
-
-const onChangeMembership = () => {
-  slide.value?.$el.getActiveIndex().then((index: number) => {
-    console.log({index});
-    changeMembershipModal.value?.present({ currentPlan, newPlan: plans.value[index] });
-  });
-}
-
-const selectMembership = (plan: any) => {
-  console.log("********");
-}
-const confirmChange = (newPlan: any) => {
-  isLoading.value = true;
-  console.log(newPlan.value);
-  console.log({InAppPurchase2});
-  console.log("[IAP] verbosity: " + InAppPurchase2.verbosity);
-  InAppPurchase2.order(newPlan.value)
-    .then(() => {
-      isLoading.value = false;
-      console.log(
-        "entered into Apple/Google pop up with the purchase confirmation"
-      );
-    })
-    .error((error: any) => {
-      isLoading.value = false;
-      errorMessage.value = `Failed to purchase: ${error}`;
-    });
-}
 </script>
 
 <style scoped lang="scss">
-
-.plans {
-	display: flex;
-	justify-content: center;
-	gap: 1rem;
-	margin-top: 1rem;
-	position: relative;
-	.radiobutton {
-		max-width: 340px;
-		--min-height: 100%;
-		width: 33%;
-		font-size: 14px;
-		line-height: 1.5;
-		position: relative;
-		--padding-top: 0;
-		--border-radius: 8px;
-		--padding-bottom: 8px;
-		--border-radius: 4px;
-		--color: var(--ion-color-medium);
-		--inner-padding: 0;
-		--inner-height: 100%;
-		--background: var(--gray-700);
-		--border-width: 0.8px;
-		--border-style: solid;
-		--border-color: var(--gray-600);
-	
-		&:not(:last-child) {
-			margin-bottom: 16px;
-		}
-	
-		&.item-radio-checked {
-			--color: var(--ion-color-white);
-			--border-color: var(--ion-color-primary);
-		}
-	
-		ion-radio {
-			width: 18px;
-			height: 18px;
-			border-width: 1.8px;
-			margin: 3px 3px 3px 12px;
-			--color: var(--ion-color-medium);
-			--mark-width: 12px;
-			--mark-height: 9px;
-			position: absolute;
-			top: 50%;
-			right: 10px;
-	
-			&::part(mark) {
-				background-position: 50% 50%;
-				background-repeat: no-repeat;
-				width: calc(100% + var(--border-width));
-				height: calc(100% + var(--border-width));
-				background-size: var(--mark-width) var(--mark-height);
-				background-image: url(/public/assets/icon/check-mark.svg);
-			}
-		}
-	
-		&__block {
-			padding: 2rem 1rem;
-			display: flex;
-			flex-direction: column;
-			align-items: center;
-			width: 100%;
-			height: 100%;
-		}
-	
-		&__icon {
-			width: 120px;
-			height: 120px;
-			padding: 1.875rem;
-			display: flex;
-			flex-shrink: 0;
-			line-height: 1;
-			font-size: 60px;
-			border-radius: 10px;
-			align-items: center;
-			justify-content: center;
-			color: var(--ion-color-white);
-			box-shadow: inset 0 0 0 0.8px var(--gray-600);
-	
-			// .radiobutton:nth-last-child(1) & {
-			// 	color: var(--ion-color-primary);
-			// }
-	
-			// .radiobutton:nth-last-child(2) & {
-			// 	color: var(--ion-color-medium);
-			// }
-	
-			// .radiobutton:nth-last-child(3) & {
-			// 	color: var(--bronze);
-			// }
-		}
-
-    .tax-text {
-      color: #ffffff6a;
-      font: 12px/1 var(--ion-font-family);
-      margin-top: 92px;
-    }
-		.gold {
-			color: var(--gold);
-		}
-		.silver {
-			color: var(--silver);
-		}
-		.bronze {
-			color: var(--bronze);
-		}
-	
-		&__description {
-			flex: 1 1 auto;
-		}
-	
-		&__label {
-			text-align: center;
-			--color: var(--ion-color-white);
-			font-family: Yantramanav;
-			font-size: 16px;
-			font-style: normal;
-			font-weight: 500;
-			line-height: 150%;
-			margin: 12px 0 8px;
-		}
-	
-		&__title {
-			display: flex;
-			align-items: center;
-		}
-	
-		&__rating {
-			display: inline-block;
-			padding: 0 9px;
-			background: var(--ion-color-primary);
-			color: var(--gray-700);
-			border-radius: 20px;
-		}
-	
-		&__cost {
-			text-align: center;
-			display: block;
-			margin-bottom: 4px;
-			font-weight: 500;
-			font-size: 16px;
-			line-height: 1.5;
-			text-transform: uppercase;
-			color: var(--ion-color-primary);
-	
-			span {
-				text-transform: none;
-				color: var(--ion-color-medium);
-			}
-		}
-	
-		ul {
-			margin: 0;
-			padding: 0;
-			list-style: none;
-			font-weight: 400;
-			font-size: 14px;
-			line-height: 1.5;
-			color: var(--ion-color-medium);
-	
-			li {
-				margin-bottom: 4px;
-	
-				&.accessibility {
-					color: var(--ion-color-primary);
-					display: flex;
-					gap: 0.5rem;
-					align-items: center;
-
-					ion-icon {
-						width: 16px;
-					}
-				}
-			}
-	
-			ion-icon {
-				font-size: 10px;
-				margin-right: 6px;
-			}
-		}
-	}
-	
-	@media (max-width: 992px) {
-		.radiobutton {
-			width: 80%;
-			max-width: unset;
-		}
-	}
-}
-
-.prev {
-	position: absolute;
-	left: 0;
-	top: calc(50% - 12px);
-	width: 24px;
-	height: 24px;
-	z-index: 10;
-}
-.next {
-	position: absolute;
-	right: 0;
-	top: calc(50% - 12px);
-	width: 24px;
-	height: 24px;
-	z-index: 10;
-}
-
-.plan-tablet {
-	@media (max-width: 992px) {
-		flex-direction: column;
-		align-items: center;
-	}
-}
 .membership {
   padding: 24px;
-  margin-top: 64px;
-
-  .membership-buttons {
-    padding-top: 48px;
-
-    ion-button#change {
-      --background: var(--beige);
-      --border-radius: 12px;
-      --color: #000000;
-      font: 700 16px/1 var(--ion-font-family);
-    }
-    ion-button#cancel {
-      --background: #DBB582;
-      --border-radius: 12px;
-      --color: var(--gray-700);
-      font: 700 16px/1 var(--ion-font-family);
-    }
-  }
 }
 
 .radiobutton {
@@ -630,17 +436,15 @@ const confirmChange = (newPlan: any) => {
     color: var(--ion-color-white);
     box-shadow: inset 0 0 0 0.8px var(--gray-600);
 
-    .radiobutton:nth-last-child(1) & {
-      color: var(--ion-color-primary);
-    }
-
-    .radiobutton:nth-last-child(2) & {
-      color: var(--ion-color-medium);
-    }
-
-    .radiobutton:nth-last-child(3) & {
-      color: var(--bronze);
-    }
+    .gold {
+			color: var(--gold);
+		}
+		.silver {
+			color: var(--silver);
+		}
+		.bronze {
+			color: var(--bronze);
+		}
   }
 
   &__description {
@@ -706,7 +510,13 @@ const confirmChange = (newPlan: any) => {
     }
   }
 }
-
+.status-text {
+  color: var(--gray-700);
+  background-color: #E1DBC5;
+  border-radius: 20px;
+  font: 500 12px/1 Lato;
+  padding: 2px 9px 2px 9px;
+}
 .holder-button {
   backdrop-filter: blur(10px);
   background: rgba(var(--ion-color-gray-900-rgb), 0.88);
