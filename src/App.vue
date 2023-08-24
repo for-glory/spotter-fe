724e6701c10fb355795a232ff2384a02dc94bbe8
Abu Saleh Muhammad Shaon <srabon.php@gmail.com>
2023-08-24 23:24:57 +0600

    Fixed status error on console

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@ onMounted(async () => {
   getMyProfile();
 
   if (!isPlatform("desktop")) {
-    await StatusBar.setStyle({ style: Style.Dark });
+    StatusBar.setStyle({ style: Style.Dark });
   }
 
   const { registerPushNotifications } = usePushNotifications();
