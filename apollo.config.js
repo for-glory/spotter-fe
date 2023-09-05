054cb2957f0c8697036d656a256d77a21241a3d7
Abu Saleh Muhammad Shaon <srabon.php@gmail.com>
2023-09-05 20:38:52 +0600

    Fixed api path url

--- apollo.config.js
+++ apollo.config.js
@@ -3,7 +3,7 @@ module.exports = {
     service: {
       name: "Spotter",
       // URL to the GraphQL API
-      url: "https://dev.spotterfitness.com/graphql",
+      url: "https://api.spotterfitness.com/graphql",
     },
     // Files processed by the extension
     includes: ["src/**/*.vue", "src/**/*.js"],
