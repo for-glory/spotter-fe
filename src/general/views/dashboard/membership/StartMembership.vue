bfaff212406394f2463d59cf9f36135b108ad66a
Abu Saleh Muhammad Shaon <srabon.php@gmail.com>
2023-09-13 18:33:50 +0600

    Sign up flow fix

--- src/general/views/dashboard/membership/StartMembership.vue
+++ src/general/views/dashboard/membership/StartMembership.vue
@@ -1,50 +1,60 @@
 <template>
-	<div class="page-content">
-		<div class="plan">
-			<ion-title class="font-20" color="primary">
-				Start with 30 days free trial
-			</ion-title>
-			<ion-title class="font-40 white-text" color="primary">
-				Choose your plan
-			</ion-title>
-			<div class="plan-features">
-				<div class="plan-features__item">
-					<ion-icon src="assets/icon/check-mark.svg" color="primary" />
-					<ion-text>All Features</ion-text>
-				</div>
-				<div class="plan-features__item">
-					<ion-icon src="assets/icon/check-mark.svg" color="primary" />
-					<ion-text>Other Features</ion-text>
-				</div>
-				<div class="plan-features__item">
-					<ion-icon src="assets/icon/check-mark.svg" color="primary" />
-					<ion-text>Premium Access</ion-text>
-				</div>
-			</div>
-			<ion-text class="font-20" color="primary">
-				Enjoy full feature with the right plan
-			</ion-text>
-			<ion-text class="font-20 grey-text">
-				You currently do not have an active plan, click next to get started
-			</ion-text>
-		</div>
-		<div class="buttons">
-			<ion-button
-				expand="block"
-				@click="handleContinue"
-			>
-				Next
-			</ion-button>
-		</div>
-	</div>
+  <div class="page-content">
+    <div class="plan">
+      <ion-title class="font-20" color="primary">
+        Start with 30 days free trial
+      </ion-title>
+      <ion-title class="font-40 white-text" color="primary">
+        Choose your plan
+      </ion-title>
+      <div class="plan-features">
+        <div class="plan-features__item">
+          <ion-icon src="assets/icon/check-mark.svg" color="primary" />
+          <ion-text>All Features</ion-text>
+        </div>
+        <div class="plan-features__item">
+          <ion-icon src="assets/icon/check-mark.svg" color="primary" />
+          <ion-text>Other Features</ion-text>
+        </div>
+        <div class="plan-features__item">
+          <ion-icon src="assets/icon/check-mark.svg" color="primary" />
+          <ion-text>Premium Access</ion-text>
+        </div>
+      </div>
+      <ion-text class="font-20" color="primary">
+        Enjoy full feature with the right plan
+      </ion-text>
+      <ion-text class="font-20 grey-text">
+        You currently do not have an active plan, click next to get started
+      </ion-text>
+    </div>
+    <div class="buttons">
+      <ion-button expand="block" @click="handleContinue"> Next </ion-button>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { IonTitle, IonText, IonImg, IonButton, IonIcon, IonGrid } from "@ionic/vue";
+import {
+  IonTitle,
+  IonText,
+  IonImg,
+  IonButton,
+  IonIcon,
+  IonGrid,
+} from "@ionic/vue";
 import { useRouter } from "vue-router";
 import { EntitiesEnum } from "@/const/entities";
+import { useFacilityStore } from "@/general/stores/useFacilityStore";
 
 const router = useRouter();
+const currentFacility = useFacilityStore();
+
+if (currentFacility?.facility?.id) {
+  router.push({
+    name: EntitiesEnum.DashboardMembership,
+  });
+}
 
 const onBack = () => {
   router.go(-1);
@@ -61,8 +71,8 @@ const handleContinue = () => {
 .page-content {
   display: flex;
   flex-direction: column;
-	align-items: center;
-	height: 100%;
+  align-items: center;
+  height: 100%;
 }
 
 .title {
@@ -77,27 +87,27 @@ const handleContinue = () => {
 }
 
 .plan {
-	margin-top: 1.5rem;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	gap: 1.5rem;
-	.plan-features {
-		display: flex;
-		flex-direction: column;
-		gap: 5px;
-		width: fit-content;
-	
-		&__item {
-			display: flex;
-			gap: 1rem;
-		}
-	}
+  margin-top: 1.5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.5rem;
+  .plan-features {
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    width: fit-content;
+
+    &__item {
+      display: flex;
+      gap: 1rem;
+    }
+  }
 }
 
 .buttons {
-	width: 40%;
-	margin-top: 10vh;
+  width: 40%;
+  margin-top: 10vh;
   .button {
     margin: 0;
     text-align: center;
